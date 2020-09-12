f6ccc486e6e260b068b9861ef7df17601ba50150
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-12 11:35:07 -0500

    The nav component was added with items to the new modules.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
   ],
   exports: [
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
