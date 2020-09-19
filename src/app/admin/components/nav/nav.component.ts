05019a84f73d87d7428fdaa923fcd0797dab0b66
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-19 17:22:07 -0500

    The signout funtionality was created.

--- src/app/admin/components/nav/nav.component.ts
+++ src/app/admin/components/nav/nav.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from "../../../core/services/auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-nav',
@@ -16,9 +18,17 @@ export class NavComponent implements OnInit {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  private authListenerSubs: Subscription
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  onLogout() {
+    this.authService.logout()
   }
 
 }
