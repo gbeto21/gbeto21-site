01d3a73606e55bc242f49f1889e724b0dd010215
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-27 15:06:35 -0500

    The footer component was created.

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -6,12 +6,20 @@ import { MaterialModule } from "../material/material.module";
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from "./about/about.component";
 import { LayoutComponent } from "../layout/layout.component";
+import { FooterComponent } from "../footer/footer.component";
 import { IntroduceComponent } from './introduce/introduce.component';
 import { SkillComponent } from "./skill/skill.component";
 import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
-  declarations: [AboutComponent, LayoutComponent, IntroduceComponent, SkillComponent, SkillsComponent],
+  declarations: [
+    AboutComponent,
+    LayoutComponent,
+    IntroduceComponent,
+    SkillComponent,
+    SkillsComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     AboutRoutingModule,
