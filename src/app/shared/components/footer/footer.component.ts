e22763e08ec922b1d3e298b982cfbc2306ffc494
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-03 13:40:54 -0500

    Styles were added to the images in the footer.

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from "src/environments/environment";
 
+const URLIMAGES = `${environment.apiUrl}/images`
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  angular = `${URLIMAGES}/angular.svg`
+  node = `${URLIMAGES}/nodejs.svg`
+  express = `${URLIMAGES}/expressjs.svg`
+  graphql = `${URLIMAGES}/graphql.svg`
+  mongodb = `${URLIMAGES}/mongodb.svg`
 
   constructor() { }
 
