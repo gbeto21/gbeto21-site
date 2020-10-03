a56c88ea48cc7c2fb47b087eca44e81cb7624542
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-03 11:35:09 -0500

    Styles were added to the footer.

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,30 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from "src/environments/environment";
 
-const URLIMAGES = `${environment.apiUrl}/images`
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  links = [
-    {
-      link: 'https://www.facebook.com/gbeto21/',
-      image: `${URLIMAGES}/facebook.png`,
-    },
-    {
-      link: 'https://twitter.com/gbeto21',
-      image: `${URLIMAGES}/twitter.png`,
-    },
-    {
-      link: 'https://github.com/gbeto21',
-      image: `${URLIMAGES}/gitHub.png`,
-    }
-
-  ]
 
   constructor() { }
 
