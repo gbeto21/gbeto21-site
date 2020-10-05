cbe31548a56609063939dbef1bdab6b0b0bcfa0d
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-05 07:41:03 -0500

    Loading strategys were added, from the user perspective.

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuicklinkStrategy } from "ngx-quicklink";
 import { NavComponent } from "./components/nav/nav.component";
 
 const routes: Routes = [
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from "./../material/material.module";
+import { QuicklinkModule } from "ngx-quicklink";
 
+import { MaterialModule } from "./../material/material.module";
 import { AdminRoutingModule } from './admin-routing.module';
 import { NavComponent } from './components/nav/nav.component';
 
@@ -10,7 +11,8 @@ import { NavComponent } from './components/nav/nav.component';
   imports: [
     CommonModule,
     AdminRoutingModule,
-    MaterialModule
+    MaterialModule,
+    QuicklinkModule
   ]
 })
 export class AdminModule { }
